a9bd4044a0ba7af70aeea59598c9b7c9d88e204a
NicoG2023 <nguevarah@udistrital.edu.co>
2025-02-05 01:48:28 -0500

    Backend finalizado

---
+++ users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import UserManager
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
