24852748623d5f71786e02c4d6517ff44840b107
NicoG2023 <nguevarah@udistrital.edu.co>
2025-02-05 02:29:29 -0500

    Agregado de dependencias de desarrollo para el proyecto

--- icard/settings.py
+++ icard/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 import datetime
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,14 +79,11 @@ WSGI_APPLICATION = 'icard.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'carta_digital'),
-        'USER': os.getenv('DB_USER', 'nicog'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'your-local-password'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # Password validation
